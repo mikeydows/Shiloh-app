044d169349c0b3f8eee9d96a2b7a3d8635ebe16e
Michael Okang <michaelokang8@gmail.com>
2026-02-08 22:12:34 +0000

    edit

--- port.js
+++ port.js
@@ -1,64 +1,79 @@
 const navbar = document.getElementById("navbar");
 const overlay = document.getElementById("overlay");
- 
-function openNavbar(){
+
+function openNavbar() {
+    navbar.style.display = "block";
     navbar.classList.remove("close");
     navbar.classList.add("open");
-     overlay.style.display = "block";
+    overlay.style.display = "block";
 }
 
-function closenavbar(){
+function closenavbar() {
     navbar.classList.remove("open");
-    navbar.classList.remove("close");
+    navbar.classList.add("close");
     overlay.style.display = "none";
 }
 
 overlay.addEventListener("click", closenavbar);
 
 navbar.addEventListener("animationend", () => {
-    if(navbar.classList.contains("close")){
+    if (navbar.classList.contains("close")) {
         navbar.style.display = "none";
     }
-})
+});
 
+/* ===== Swipe logic ===== */
 let startX = 0;
+let moveX = 0;
+
 navbar.addEventListener("touchstart", (e) => {
     startX = e.touches[0].clientX;
-    let diff = startX - moveX;]\                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+});
 
-    if(diff > 70){
+navbar.addEventListener("touchmove", (e) => {
+    moveX = e.touches[0].clientX;
+});
+
+navbar.addEventListener("touchend", () => {
+    const diff = startX - moveX;
+
+    // swipe left to close
+    if (diff > 70) {
         closenavbar();
     }
-})
+});
+
+/* Swipe from left edge to open */
 document.addEventListener("touchstart", (e) => {
-    if(e.touches[0].clientX < 30){
+    if (e.touches[0].clientX < 30) {
         openNavbar();
     }
-})
-
+});
 
 const photo = document.getElementById("photo");
 const graphic = document.getElementById("graphic");
-const web = document.getElementById("web");
 const photography = document.getElementById("photography");
-const graphicPage = document.getElementById("graphicdesign")
+const graphicPage = document.getElementById("graphicdesign");
+const heading = document.getElementById("head");
 
-function showPhotographyPage(){
-    if(window.getComputedStyle(photography) === "none"){
-        photo.style.backgroundColor = "black";
-        photography.style.display = "flex";
-        graphicPage.style.display = "none";
-    }
+function showPhotographyPage() {
+    photo.style.backgroundColor = "black";
+    graphic.style.backgroundColor = "";
+
+    photography.style.display = "flex";
+    heading.innerHTML = "Photography"
+    graphicPage.style.display = "none";
 }
 
-photo.addEventListener("click", showPhotographyPage)
+function showGraphicPage() {
+    graphic.style.backgroundColor = "black";
+    photo.style.backgroundColor = "";
 
-function showGraphicPage(){
-    if(window.getComputedStyle(graphicPage) === "none"){
-        graphic.style.backgroundColor = "black";
-        graphicPage.style.display = "flex";
-        photographye.style.display = "none";
-    }
+    heading.innerHTML = "Graphic Design"
+
+    graphicPage.style.display = "flex";
+    photography.style.display = "none";
 }
 
-graphic.addEventListener("click", showGraphicPage)
+photo.addEventListener("click", showPhotographyPage);
+graphic.addEventListener("click", showGraphicPage);
